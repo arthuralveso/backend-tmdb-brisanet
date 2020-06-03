6c9e948d4c893dc74ccd2864a302250c372a8d2b
Arthur Alves <arthuralves91@hotmail.com>
2020-06-03 01:13:16 -0300

    variaveis de ambiente

--- src/app/controllers/GuestSessionController.js
+++ src/app/controllers/GuestSessionController.js
@@ -1,26 +1,31 @@
 const jwt = require('jsonwebtoken');
 const authConfig = require('../../config/auth');
 const api = require('../../services/api');
+require('dotenv/config');
 
 class GuestSessionController {
   async index(request, response, next) {
-    const guestSession = await api
-      .get('authentication/guest_session/new', {
-        params: {
-          api_key: '4128b7117a353c53f0e30496fa69220b',
-        },
-      })
-      .then((res) => res);
+    try {
+      const guestSession = await api
+        .get('authentication/guest_session/new', {
+          params: {
+            api_key: process.env.API_KEY,
+          },
+        })
+        .then((res) => res);
 
-    const sessionData = guestSession.data;
-    const id = sessionData.guest_session_id;
+      const sessionData = guestSession.data;
+      const id = sessionData.guest_session_id;
 
-    return response.json({
-      sessionData,
-      token: jwt.sign({ id }, authConfig.secret, {
-        expiresIn: authConfig.expiresIn,
-      }),
-    });
+      return response.json({
+        sessionData,
+        token: jwt.sign({ id }, authConfig.secret, {
+          expiresIn: authConfig.expiresIn,
+        }),
+      });
+    } catch (error) {
+      response.status(401).json({ error: 'Request failed' });
+    }
   }
 }
 
--- src/app/controllers/MovieController.js
+++ src/app/controllers/MovieController.js
@@ -2,98 +2,114 @@ const api = require('../../services/api');
 
 class MovieController {
   async indexById(request, response, next) {
-    const { id } = request.params;
+    try {
+      const { id } = request.params;
 
-    const movieResponse = await api
-      .get(`movie/${id}`, {
-        params: {
-          api_key: '4128b7117a353c53f0e30496fa69220b',
-          language: 'pt-BR',
-        },
-      })
-      .then((res) => res);
+      const movieResponse = await api
+        .get(`movie/${id}`, {
+          params: {
+            api_key: process.env.API_KEY,
+            language: 'pt-BR',
+          },
+        })
+        .then((res) => res);
 
-    const {
-      original_title,
-      poster_path,
-      backdrop_path,
-      genres,
-      overview,
-      release_date,
-    } = movieResponse.data;
+      const {
+        original_title,
+        poster_path,
+        backdrop_path,
+        genres,
+        overview,
+        release_date,
+      } = movieResponse.data;
 
-    const movie = {
-      original_title,
-      overview,
-      poster_path,
-      backdrop_path,
-      release_date,
-      genres,
-    };
+      const movie = {
+        original_title,
+        overview,
+        poster_path,
+        backdrop_path,
+        release_date,
+        genres,
+      };
 
-    return response.json(movie);
+      return response.json(movie);
+    } catch (error) {
+      response.status(401).json({ error: 'Request failed' });
+    }
   }
 
   async index(request, response, next) {
-    const { genre, actor } = request.query;
+    try {
+      const { genre, actor } = request.query;
 
-    const movieResponse = await api
-      .get('discover/movie', {
-        params: {
-          api_key: '4128b7117a353c53f0e30496fa69220b',
-          language: 'pt-BR',
-          region: 'BR',
-          with_genres: genre,
-          with_people: actor,
-        },
-      })
-      .then((res) => res);
+      const movieResponse = await api
+        .get('discover/movie', {
+          params: {
+            api_key: process.env.API_KEY,
+            language: 'pt-BR',
+            region: 'BR',
+            with_genres: genre,
+            with_people: actor,
+          },
+        })
+        .then((res) => res);
 
-    const movieList = movieResponse.data;
+      const movieList = movieResponse.data;
 
-    return response.json(movieList);
+      return response.json(movieList);
+    } catch (error) {
+      response.status(401).json({ error: 'Request failed' });
+    }
   }
 
   async indexByName(request, response, next) {
-    const { name } = request.query;
+    try {
+      const { name } = request.query;
 
-    const movieResponse = await api
-      .get('search/movie', {
-        params: {
-          api_key: '4128b7117a353c53f0e30496fa69220b',
-          language: 'pt-BR',
-          query: name,
-          region: 'BR',
-        },
-      })
-      .then((res) => res);
+      const movieResponse = await api
+        .get('search/movie', {
+          params: {
+            api_key: process.env.API_KEY,
+            language: 'pt-BR',
+            query: name,
+            region: 'BR',
+          },
+        })
+        .then((res) => res);
 
-    const movie = movieResponse.data;
+      const movie = movieResponse.data;
 
-    return response.json(movie);
+      return response.json(movie);
+    } catch (error) {
+      response.status(401).json({ error: 'Request failed' });
+    }
   }
 
   async store(request, response, next) {
-    const { rate } = request.body;
-    const { id } = request.params;
-    const { session_id } = request.query;
+    try {
+      const { rate } = request.body;
+      const { id } = request.params;
+      const { session_id } = request.query;
 
-    const movieResponse = await api
-      .post(
-        `/movie/${id}/rating`,
-        { value: rate },
-        {
-          params: {
-            api_key: '4128b7117a353c53f0e30496fa69220b',
-            guest_session_id: session_id,
-          },
-        }
-      )
-      .then((res) => res);
+      const movieResponse = await api
+        .post(
+          `/movie/${id}/rating`,
+          { value: rate },
+          {
+            params: {
+              api_key: process.env.API_KEY,
+              guest_session_id: session_id,
+            },
+          }
+        )
+        .then((res) => res);
 
-    const movie = movieResponse.data;
+      const movie = movieResponse.data;
 
-    return response.json(movie);
+      return response.json(movie);
+    } catch (error) {
+      response.status(401).json({ error: 'Request failed' });
+    }
   }
 }
 
