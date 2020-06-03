2f8808fc10ede7810b4e1e2ef270c883d4913c47
Arthur Alves <arthuralves91@hotmail.com>
2020-06-03 00:36:17 -0300

    verificaçao do token

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -1,8 +1,20 @@
-module.exports = (req, res, next) => {
+const jwt = require('jsonwebtoken');
+const { promisify } = require('util');
+const authConfig = require('../../config/auth');
+
+module.exports = async (req, res, next) => {
   const authHeader = req.headers.authorization;
 
   if (!authHeader) {
     return res.status(401).json({ error: 'unauthorized, token not provided' });
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    await promisify(jwt.verify)(token, authConfig.secret);
+  } catch (err) {
+    return res.status(401).json({ error: 'Token invalid' });
   }
 
   return next();
